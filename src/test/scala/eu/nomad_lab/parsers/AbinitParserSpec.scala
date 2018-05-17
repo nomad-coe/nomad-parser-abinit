6292dabaaa058ccfb56b112809daa7eff2d5255a
Micael Oliveira <micael.oliveira@mpsd.mpg.de>
2018-05-17 15:22:18 +0200

    Correct copyright holders for scala files.

--- src/test/scala/eu/nomad_lab/parsers/AbinitParserSpec.scala
+++ src/test/scala/eu/nomad_lab/parsers/AbinitParserSpec.scala
@@ -1,5 +1,5 @@
 /*
-   Copyright 2016-2017 The NOMAD Developers Group
+   Copyright 2016-2018 Micael Oliveira, Fawzi Mohamed
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
