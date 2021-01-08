e2c97b91a97ff997b41a99df9595ba6faea15f36
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2021-01-08 04:24:04 +0100

    New parser based on textparser

--- abinitparser/__init__.py
+++ abinitparser/__init__.py
@@ -11,22 +11,4 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-from .metainfo import m_env
-from nomad.parsing.parser import FairdiParser
-from abinitparser.parser2 import AbinitParserInterface
-
-
-
-class AbinitParser(FairdiParser):
-    def __init__(self):
-        super().__init__(
-        name='parsers/abinit', code_name='ABINIT', code_homepage='https://www.abinit.org/',
-        mainfile_contents_re=(r'^\n*\.Version\s*[0-9.]*\s*of ABINIT\s*'))
-
-    def parse(self, filepath, archive, logger=None):
-        self._metainfo_env = m_env
-
-        parser = AbinitParserInterface(filepath, archive, logger)
-
-        parser.parse()
+from abinitparser.abinit_parser import AbinitParser
